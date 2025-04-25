af405c16fc30d92383af7cfee7cf162b8eb926ad
Syberiano <syberiano.2704@gmail.com>
2025-04-25 14:38:16 -0500

    Implement code changes to enhance functionality and improve performance

--- main/main.java
+++ main/main.java
@@ -1,6 +1,7 @@
-// Created by: [Jhon Correa]
-// Date: 2025-04-24
-
-public class main {
+public static main(String[] args) {   
     
-}
+    System.out.println("Hello, World!");
+    System.out.println("This is a simple Java program.");
+    System.out.println("It prints a message to the console.");
+    System.out.println("Goodbye!");
+ }
