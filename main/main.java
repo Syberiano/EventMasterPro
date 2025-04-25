a61617292691359300357733505ca363c834341f
Syberiano <syberiano.2704@gmail.com>
2025-04-25 14:42:58 -0500

    Corregir la declaración del método main en main.java
    errores

--- main/main.java
+++ main/main.java
@@ -1,4 +1,4 @@
-public static main(String[] args) {   
+public static void main(String[] args) {   
     
     System.out.println("Hello, World!");
     System.out.println("This is a simple Java program.");
